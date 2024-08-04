07f9e5ea2cefdcf0d0c7c1608116ddaef64fe509
r3quie <r3quie@gmail.com>
2024-08-04 18:50:47 +0200

    Oliver Twist

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"archive/zip"
+	"io"
+	"regexp"
+	"strings"
+)
+
+// Returns text from a doc or docx file as string
+func readDocx(src string) (string, error) {
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		return "", err
+	}
+	defer func() {
+		if err := r.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	for _, f := range r.File {
+		if f.Name == "word/document.xml" {
+			// fmt.Println("found")
+			doc, errdoc := f.Open()
+			if errdoc != nil {
+				return "", errdoc
+			}
+			buf := new(strings.Builder)
+			_, errd := io.Copy(buf, doc)
+			if errd != nil {
+				return "", errdoc
+			}
+			reg, _ := regexp.Compile(`\<.*?\>`)
+			return reg.ReplaceAllString(buf.String(), ""), nil
+
+		}
+	}
+	return "", nil
+}
+
+func search(term string, path string) (bool, error) {
+	text, err := readDocx(path)
+	if err != nil {
+		return false, err
+	}
+	return strings.Contains(text, term), nil
+}
+
+func main() {
+
+}
